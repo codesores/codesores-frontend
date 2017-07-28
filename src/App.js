1787266cec554a1214a3057f4f6b29d9958351a6
john-michael <j.johnmichaelmurphy@gmail.com>
2017-07-28 08:41:50 -0700

    add axios as HTTP client.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import issueShow from './views/issues/show.js';
+import Axios from 'axios'
 
 import {
   BrowserRouter as Router,
--- src/views/issues/show.js
+++ src/views/issues/show.js
@@ -6,24 +6,36 @@ import Meta from '../../components/issue_components/meta.js'
 import Vote from '../../components/issue_components/vote.js'
 import ButtonContribute from '../../components/issue_components/button_contribute.js'
 
-const issueShow = () => (
-  <div>
+class issueShow extends React.Component {
 
-  <div id='issue_id'>
-  <Comments />
-  <Summary />
-  <Meta />
-  </div>
+  constructor(args){
+    super(args)
 
-  <div id='feedback'>
-  <Vote />
-  </div>
+    // axios()
+  }
 
-  <div id='contribute'>
-  <ButtonContribute />
-  </div>
+  render(){
+    return(
+      <div>
 
-  </div>
-)
+      <div id='issue_id'>
+      <Comments />
+      <Summary />
+      <Meta />
+      </div>
+
+      <div id='feedback'>
+      <Vote />
+      </div>
+
+      <div id='contribute'>
+      <ButtonContribute />
+      </div>
+
+      </div>
+    )
+  }
+}
+
 
 export default issueShow;
