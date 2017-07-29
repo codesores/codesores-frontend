40885738fe92bd94b79cd35a9cc880d9c52d182e
Josh Leichtung <joshleichtung@gmail.com>
2017-07-29 16:35:28 -0700

    Change App.js so that App component is a class

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
 
-const App = () => (
-  <div>
-    <Header />
-    <Main />
-  </div>
-)
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+      </div>
+    )
+  }
+}
 
 export default App
