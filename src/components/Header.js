f4e639009fcbd4f764771c4a59fb2104ef1ad1f4
Gustavo Amaral <gustavoamaral@Gustavos-MacBook-Pro.local>
2017-07-30 15:31:28 -0700

    added boilerplate top repos to welcome page

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ class Header extends Component {
         <li>
         { this.logInOrLogout() }
         </li>
-        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/home'>Home</Link></li>
       </ul>
     </nav>
   </header>
