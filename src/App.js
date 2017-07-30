07f90e0652b8d1984f52923656fdce277489b840
Josh Leichtung <joshleichtung@gmail.com>
2017-07-29 17:40:51 -0700

    Add ability to store session data in state of app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
+import axios from 'axios'
+import { getQueryParams } from './utils';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    const params = getQueryParams();
+    this.state = { token: params.token };
+  }
   render() {
     return (
       <div>
