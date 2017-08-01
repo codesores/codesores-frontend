0b147d3958a09592afb7db76fb1186544c496950
Gustavo Amaral <gustavoamaral@Gustavos-MacBook-Pro.local>
2017-08-01 11:32:53 -0700

    sort feedback by newest, also added limit function

--- src/components/profile_components/PastFeedback.js
+++ src/components/profile_components/PastFeedback.js
@@ -3,40 +3,46 @@ import { Link } from 'react-router-dom'
 
 
 class PastFeedback extends Component {
-  constructor(){
-    super()
-
+ 
+  sortRecentFeedbacks(feedbacks){
+    return feedbacks.sort((a, b)=>{
+      return new Date(b.created_at) - new Date(a.created_at);
+    });
   }
 
   iterateFeedbacks(limit=10){
+    let sortedFeedback = this.sortRecentFeedbacks(this.props.feedbacks)
 
+    let cappedFeedback = sortedFeedback.slice(0, limit).map((feedback) => {
+      const issueUrl = "/issues/" + feedback.issue_id
+      return (
+        <li item={feedback} key={feedback.id}>
+        <Link to={issueUrl}>Issue</Link>
+        <div>Validity: {feedback.validity}</div>
+        <div>Difficulty: {feedback.difficulty}</div>
+        </li>
+        )
+    })
+
+    return cappedFeedback
+  }
+
+
+  feedbackBox(){
     if (this.props.feedbacks){
-      let feedback_list = this.props.feedbacks.slice(0, limit).map((feedback) => {
-        const issueUrl = "/issues/" + feedback.issue_id
-        return (
-          <li item={feedback} key={feedback.id}>
-            <Link to={issueUrl}>Issue</Link>
-            <div>Validity: {feedback.validity}</div>
-            <div>Difficulty: {feedback.difficulty}</div>
-          </li>
-          )
-      })
-
       return (
         <div>
         <p> Past Feedbacks: </p>
-        {feedback_list}
+        {this.iterateFeedbacks(5)}
         </div>
         )
     }
-
   }
 
   render() {
-    console.log(this.props)
     return (
       <div>
-      {this.iterateFeedbacks()}
+      {this.feedbackBox()}
       </div>
       );
   }
--- src/views/users/show.js
+++ src/views/users/show.js
@@ -7,14 +7,8 @@ import PastFeedback from '../../components/profile_components/PastFeedback.js'
 
 class UsersShow extends React.Component {
 
-  constructor(){
-    super()
-  }
-
-
   render(){
     const info = this.props.info
-    console.log(info)
 
     return(
       <Grid>
@@ -22,12 +16,12 @@ class UsersShow extends React.Component {
             <Col xs={12} md={8}>
               <div id='issue_id'>
                 <h1> {info.name} </h1>
-                <PastFeedback feedbacks={info.user_feedbacks}/>
               </div>
             </Col>
             <Col xs={6} md={4}>
               <div id='feedback'><br/>
                 <a href={info.html_url}>GitHub Page</a><br/><br/>
+                <PastFeedback feedbacks={info.user_feedbacks}/>
               </div>
             </Col>
           </Row>
