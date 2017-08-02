995cd38f69f63169c94617ccd3a8f95f0639f8f8
Gustavo Amaral <gustavoamaral@Gustavos-MacBook-Pro.local>
2017-08-02 14:31:36 -0700

    add back button, yay

--- src/index.js
+++ src/index.js
@@ -6,19 +6,20 @@ import registerServiceWorker from './registerServiceWorker';
 import {
   BrowserRouter as Router
 } from 'react-router-dom'
+import createHistory from 'history/createBrowserHistory'
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 import theme from './toolbox/theme';
 
 
 
 ReactDOM.render((
-  <Router>
-  <ThemeProvider theme={theme}>
-  <div>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
-  <App exact path='/' component={App}/>
-  </div>
-  </ThemeProvider>
+  <Router >
+    <ThemeProvider theme={theme}>
+    <div>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
+      <App exact path='/' component={App}/>
+    </div>
+    </ThemeProvider>
   </Router>
 
   ), document.getElementById('root'));
