7ad2162a8312c5751ace012b51a4a2cb309347df
Gustavo Amaral <gustavoamaral@Gustavos-MacBook-Pro.local>
2017-08-03 12:11:05 -0700

    viz for star button

--- src/components/star_components/StarButton.js
+++ src/components/star_components/StarButton.js
@@ -1,26 +1,23 @@
 import React from 'react'
 import axios from 'axios'
 import Button from 'react-toolbox/lib/button/Button'
-import liked from './liked.svg'
+import liked from './favorite.svg'
 import not_liked from './not_liked.svg'
-
 
 
 class StarButton extends React.Component {
 
-
   buttonConditional(){
     if (this.props.liked){
       return (
-      <span onClick={this.props.setStar}>
-        <img width="35px" height="25px" src={liked} color='red'/>
-        
+      <span onClick={this.props.setStar} className="like-button">
+        <img width="55px" height="55px" src={liked}/>
       </span> 
         )
     } else {
       return (
-      <span onClick={this.props.setStar}>
-        <img width="35px" height="25px" src={not_liked} color='red'  />
+      <span onClick={this.props.setStar} className="like-button" >
+        <img width="55px" height="55px" src={not_liked} />
       </span>
         )
     }
