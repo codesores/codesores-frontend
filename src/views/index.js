3dd02f4567b08fbd5be8ce8297c07a150664235c
john-michael <j.johnmichaelmurphy@gmail.com>
2017-08-03 20:25:22 -0700

    finish sort by feature

--- src/views/index.js
+++ src/views/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import SearchApp from '../components/search_components/SearchApp.js'
 import SearchResults from '../components/search_components/SearchResults.js'
-import SortBy from '../components/search_components/SortBy.js'
 import Header from '../components/Header.js'
 import LoginButton from '../components/LoginButton'
+import SortBy from '../components/search_components/SortBy.js'
 
 
 class Index extends React.Component {
@@ -18,6 +18,7 @@ class Index extends React.Component {
     this.updateResults = this.updateResults.bind(this)
     this.sortResultsBy = this.sortResultsBy.bind(this)
     this.updateSortBy = this.updateSortBy.bind(this)
+    this.updateSortByDescending = this.updateSortByDescending.bind(this)
   }
 
   updateSortBy(toggleValue){
@@ -25,17 +26,29 @@ class Index extends React.Component {
     this.sortResultsBy(toggleValue)
   }
 
+  updateSortByDescending(){
+    this.setState({sortByDescending: !this.state.sortByDescending})
+    this.sortResultsBy()
+  }
+
   updateResults(response){
     this.setState({resultsPurgatory: response})
     this.sortResultsBy()
   }
 
+
   sortResultsBy(toggleValue){
+    console.log(this.state.sortByDescending)
     let sortedArray = []
     let sortBy = toggleValue
-    if(sortBy) {
+    if(sortBy && this.state.sortByDescending) {
       sortedArray = this.state.resultsPurgatory.sort(function(a, b){
         return a[sortBy] - b[sortBy]
+      })
+    }
+    else if(sortBy && !this.state.sortByDescending) {
+      sortedArray = this.state.resultsPurgatory.sort(function(a, b){
+        return b[sortBy] - a[sortBy]
       })
     }
 
@@ -55,8 +68,8 @@ class Index extends React.Component {
       <div className='centering'><LoginButton/></div>
       <SearchApp updateResults={this.updateResults} setNotice={this.props.setNotice} token={this.props.token} />
       <main id='search-results'>
-      <SortBy updateSortBy={this.updateSortBy}/>
-      <SearchResults resultsToDisplay={this.state.resultsToDisplay} token={this.props.token} />
+      <SortBy updateSortByDescending={this.updateSortByDescending} updateSortBy={this.updateSortBy} sortByDescending={this.state.sortByDescending}/>
+      <SearchResults resultsToDisplay={this.state.resultsToDisplay} token={this.props.token} sortByDescending={this.state.sortByDescending} updateSortBy={this.updateSortBy}/>
       </main>
       </div>
     )
